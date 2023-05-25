835c18476df696ef86bd59ddfd5632829a85e559
RomanV79 <roman.vlasov19@gmail.com>
2023-05-25 20:30:51 +0300

    develop: wrote DTO class

---
+++ src/main/java/dto/ExchangeRatesDTO.java
@@ -0,0 +1,39 @@
+package dto;
+
+import entity.Currencies;
+
+public class ExchangeRatesDTO {
+    private Currencies baseCurrency;
+    private Currencies targetCurrency;
+    private double rate;
+    private double amount;
+    private double convertedAmount;
+
+    public ExchangeRatesDTO(Currencies baseCurrency, Currencies targetCurrency, double rate, double amount, double convertedAmount) {
+        this.baseCurrency = baseCurrency;
+        this.targetCurrency = targetCurrency;
+        this.rate = rate;
+        this.amount = amount;
+        this.convertedAmount = convertedAmount;
+    }
+
+    public void setBaseCurrency(Currencies baseCurrency) {
+        this.baseCurrency = baseCurrency;
+    }
+
+    public void setTargetCurrency(Currencies targetCurrency) {
+        this.targetCurrency = targetCurrency;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setConvertedAmount(double convertedAmount) {
+        this.convertedAmount = convertedAmount;
+    }
+}
